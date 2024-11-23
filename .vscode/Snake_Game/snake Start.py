637bb2a932581bd2b8ff8911bc1ff24411c26cb9
Yusu-ess <yusukun2020@gmail.com>
2024-11-23 11:11:48 +0300

    i couldn't create additional body in list i need a help but i created the super food

--- .vscode/Snake_Game/snake Start.py
+++ .vscode/Snake_Game/snake Start.py
@@ -1,9 +1,9 @@
 import turtle #importing turtle module
 import random #importing random module
 
-class screen:#create class screen
+
  
- def display_screen(self): #creating the screen
+def display_screen(self): #creating the screen
 
     wind=turtle.Screen() #activate the function
     wind.title(" welcome to snake game ") #set the title
@@ -14,36 +14,49 @@ class screen:#create class screen
 
 class Snake:#create class snake
 
-    def snake_head(self): #set snake head
-     
+    def snake_main(self): 
+     #set snake head
      snake_head=turtle.Turtle() #set the object
      snake_head.speed(0) #set snake's head speed
      snake_head.shape("circle") #set snake's head shape 
      snake_head.goto(0,0) #set snake's head coordinates
      snake_head.penup() #delete the line after moving
-     snake_head.color("red") #set snake's head color
-
-
-    def  snake_body(self):#set snake body
-     
+     snake_head.color("red") #set snake's head color 
+     #set snake body
      snake_body=turtle.Turtle() #set the object
      snake_body.speed(0) #set the snake's body speed
      snake_body.color("red") #set the snake's body color
      snake_body.shape("circle") #set the snake's body shape
-     snake_body.goto(0,0) #set the snake's body coordinates
+     snake_body.goto(2,0) #set the snake's body coordinates
      snake_body.penup() #delete the line after moving
      snake_body.shapesize(stretch_wid=1,stretch_len=5) #stretch the widths by muiltiplying 5 x 20 
-
-
-    def  snake_tail(self): #set snake tail
-     
+     #set snake tail
      snake_tail=turtle.Turtle() #set the object
      snake_tail.speed(0) #set the snake's tail speed
      snake_tail.color("red") #set the snake's tail color
      snake_tail.shape("circle") #set the snake's tail shape
-     snake_tail.goto(0,0) #set the snake's tail coordinates
+     snake_tail.goto(3,0) #set the snake's tail coordinates
      snake_tail.penup() #delete the line after moving
 
+    def snake_add_body(self):
+       add_body=[]
+       
+
+          
+          
+
+
+
+        
+     
+
+       
+        
+
+
+   
+     
+    
 
 class Food:#create class food
    
@@ -54,4 +67,14 @@ class Food:#create class food
     food.color("blue")  #set the color
     food.penup()  #delete the line after moving
     food.goto(0,100) #set cood=rdinates
+    return 1
+   
+   def super_food(self):
+      sup_food=turtle.Turtle() #set the object
+      sup_food.shape("circle") #set the shape
+      sup_food.color("blue")  #set the color
+      sup_food.penup()  #delete the line after moving
+      sup_food.goto(0,100) #set cood=rdinates
+      return 3
+      
 
